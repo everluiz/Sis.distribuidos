d48f3088f62f037be068093a27bf52609b5ef8a1
Everton Luiz Pagnoncelli <everluiz@debian>
2021-07-15 17:36:37 -0300

    bubble serial de 100.000 pontos

--- bubble_serial.c
+++ bubble_serial.c
@@ -4,27 +4,35 @@
 
 #define MAX 100000
 
+void bubble(int vet[], int inicio, int fim){
+    for (int i = fim-1; i > inicio; i--)
+    {
+        for (int j = inicio; j < i; j++)
+        {
+            if(vet[j] > vet[j+1]){
+                int aux = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
+            }
+        }
+        
+    }
+    
+}
+
 int main(void){
     int *vetor, ch;
 
     vetor = (int *) malloc(MAX * sizeof(int));
     srand((unsigned)time(NULL));
 
+    // Cria vetor randomizado de 1 a 100000.
     for (int i = 0; i < MAX; i++)
     {
         ch = 1 + ( rand() % 10000);
         vetor[i] = ch;
     }
     
-    for (int i= MAX-1; i >> 0; i--)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (vetor[j] > vetor[j+1]){
-                int aux = vetor[j];
-                vetor[j] = vetor[j+1];
-                vetor[j+1] = aux;
-            }
-        }  
-    }    
+    bubble(vetor, 0, MAX);
+    return 0;    
 }
