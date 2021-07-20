0186a038600d21fd8c351e909f4d37426a3ee08c
Everton Luiz <everluizpag@gmail.com>
2021-07-20 09:48:44 -0300

    primeira versao paralela

---
+++ bubble_parallel.c
@@ -0,0 +1,51 @@
+#include <omp.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+        int i, n, tmp, *x, changes;
+        int chunk;
+        scanf("%d ", &n);
+        chunk = n / 4;
+        x = (int*) malloc(n * sizeof(int));
+        for(i = 0; i < n; ++i)
+            scanf("%d ", &x[i]);
+    changes = 1;
+    int nr = 0;
+    while(changes)
+    {
+    #pragma omp parallel private(tmp)
+    {
+            nr++;
+            changes = 0;
+            #pragma omp for \
+                    reduction(+:changes)
+            for(i = 0; i < n - 1; i = i + 2)
+            {
+                    if(x[i] > x[i+1] )
+                    {
+                            tmp = x[i];
+                            x[i] = x[i+1];
+                            x[i+1] = tmp;
+                            ++changes;
+                    }
+            }
+            #pragma omp for \
+                    reduction(+:changes)
+            for(i = 1; i < n - 1; i = i + 2)
+            {
+                    if( x[i] > x[i+1] )
+                    {
+                            tmp = x[i];
+                            x[i] = x[i+1];
+                            x[i+1] = tmp;
+                            ++changes;
+                    }
+            }
+    }
+    }
+
+    return 0;
+}
