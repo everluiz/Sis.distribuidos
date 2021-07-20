d67da6f130fe84b0592801b7a6a2eb5c555f797a
Everton Luiz Pagnoncelli <everluiz@debian>
2021-07-20 14:37:35 -0300

    Codigo bubblesort serial e paralelo

--- bubble_serial.c
+++ bubble_serial.c
@@ -1,38 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
+#include <stdbool.h>
 
 #define MAX 100000
 
+void swap(int *valor1, int *valor2){
+    int aux = *valor1;
+    *valor1 = *valor2;
+    *valor2 = aux;
+}
+
 void bubble(int vet[], int inicio, int fim){
-    for (int i = fim-1; i > inicio; i--)
+    double start = omp_get_wtime();
+    for (int i = inicio; i < fim; i++)
     {
-        for (int j = inicio; j < i; j++)
+        bool stop = true;
+        for (int j = inicio; j < fim-1; j++)
         {
             if(vet[j] > vet[j+1]){
-                int aux = vet[j];
-                vet[j] = vet[j+1];
-                vet[j+1] = aux;
+                swap(&vet[j], &vet[j+1]);
+                stop = false;
+            }
+        }
+        if (stop)
+        {
+            break;
+        }
+        
+    }
+    double end = omp_get_wtime();
+    printf("tempo serial: %f\n",end-start);
+}
+
+
+void bubble_par(int vet[], int inicio, int fim){
+    int first;
+    double start = omp_get_wtime();
+    for (int i = 0; i < MAX; i++)
+    {
+        first = i % 2; 
+        // 0 para 0,2,4...
+        // 1 para 1,3,5...
+        #pragma omp parallel for shared(vet, first),num_threads(8) 
+        for (int j = first; j < MAX-1; j+=2)
+        {
+            if(vet[j] > vet[j+1]){
+                swap(&vet[j], &vet[j+1]);
             }
         }
         
     }
-    
+    double end = omp_get_wtime();
+    printf("tempo paralelo: %f\n",end-start);
 }
 
+
 int main(void){
-    int *vetor, ch;
+    int *vetor, *vetor2, ch;
 
     vetor = (int *) malloc(MAX * sizeof(int));
+    vetor2 = (int *) malloc(MAX * sizeof(int));
     srand((unsigned)time(NULL));
 
     // Cria vetor randomizado de 1 a 100000.
     for (int i = 0; i < MAX; i++)
     {
-        ch = 1 + ( rand() % 10000);
+        ch = 1 + ( rand() % MAX);
         vetor[i] = ch;
+        vetor2[i] = ch;
+    }
+    // chama a funcao bubblesort serial
+    //bubble(vetor, 0, MAX);
+    /*
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d ",vetor[i]);
+    }
+    */
+    printf("\n\n");
+    // chama a funcao bubblesort paralela
+    bubble_par(vetor2, 0, MAX);
+
+    
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d ", vetor2[i]);
     }
     
-    bubble(vetor, 0, MAX);
     return 0;    
 }
